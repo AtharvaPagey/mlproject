b754b619c669595d90e99727f9489da1fb4b6276
Atharva Pagey <atharvapagey045@gmail.com>
2025-09-28 11:34:43 +0530

    hyperparameter_tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -40,19 +40,65 @@ class ModelTrainer:
                 "AdaBoost Regressor" : AdaBoostRegressor(),
                 "CatBoost Regressor" : CatBoostRegressor(verbose = False),
                 "KNeighbors Regressor" : KNeighborsRegressor(),
+                "GradientBoostingRegressor" : GradientBoostingRegressor()
             }
 
-            model_report:dict = evaluate_model(X_train, y_train, X_test, y_test, models)
+            params = {
+                "Linear Regression" : {
+                    "fit_intercept" : [True, False],
+                },
+                "Decision Tree Regressor":{
+                    "criterion" : ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    "splitter" : ["best", "random"],
+                    "max_depth" : [5, 6, 8, 7, 9, 10, 2, 3],
+                    "max_features" : ["sqrt", "log2"]
+                },
+                "Random Forest Regressor" : {
+                    "n_estimators" : [100, 150, 200, 175, 300, 325],
+                    "criterion" : ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    "max_depth" : [5, 6, 8, 7, 9, 10, 2, 3],
+                    "max_features" : ["sqrt", "log2"]
+                },
+                "XGBRegressor" : {
+                    # "loss" : ['squared_error', 'absolute_error', 'huber', 'quantile'],
+                    "learning_rate" : [0.1, 0.01, 0.05, 0.15, 0.02],
+                    "n_estimators" : [100, 150, 200, 175, 300, 325],
+                    # "criterion" : ['squared_error', 'friedman_mse'],
+                    "max_depth" : [5, 6, 8, 7, 9, 10, 2, 3],
+                    # "max_features" : ["sqrt", "log2"]
+                },
+                "AdaBoost Regressor" : {
+                    "loss" : ['linear', 'square', 'exponential'],
+                    "learning_rate" : [0.1, 0.01, 0.05, 0.15, 0.02],
+                    "n_estimators" : [100, 150, 200, 175, 300, 325]
+                },
+                "CatBoost Regressor" : {
+                    "iterations" : [100, 150, 200, 175, 300, 325],
+                    "learning_rate" : [0.1, 0.01, 0.05, 0.15, 0.02]
+                },
+                "KNeighbors Regressor" : {
+                    "n_neighbors" : [5, 6, 7, 4, 3],
+                    "weights" : ['uniform', 'distance'],
+                    "algorithm" : ['auto'],
+                    "p" : [1, 2]
+                },
+                "GradientBoostingRegressor" : {
+                    "loss" : ['squared_error', 'absolute_error', 'huber', 'quantile'],
+                    "learning_rate" : [0.1, 0.01, 0.05, 0.15, 0.02],
+                    "n_estimators" : [100, 150, 200, 175, 300, 325]
+                },
+            }
 
-            best_model_score = max(sorted(model_report.values()))
+            model_report:dict = evaluate_model(X_train, y_train, X_test, y_test, models, params)
 
-            best_model_name = list(model_report.keys())[
-                list(model_report.values()).index(best_model_score)
-                ]
+            best_model_name = max(model_report, key=lambda name: model_report[name][0])
+            best_model_score = model_report[best_model_name][0]
             
             best_model = models[best_model_name]
 
-            if best_model_score < 0.6:
+            least_score = 0.6
+
+            if best_model_score < least_score:
                 raise CustomExeception("No Best Model Found")
             
             logging.info("Best model found o both training and testing")
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 from src.exception import CustomExeception
 from sklearn.metrics import r2_score
 import dill
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path, obj):
@@ -18,18 +19,24 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomExeception(e, sys)
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
         for i in range(len(list(models.values()))):
             model = list(models.values())[i]
-            model.fit(X_train, y_train)
+            param = params[list(models.keys())[i]]
 
+            cv = GridSearchCV(estimator=model, param_grid=param, n_jobs=-1, cv = 3, verbose= 3, refit=True)
+            cv.fit(X_train, y_train)
+
+            model.set_params(**cv.best_params_)
+            model.fit(X_train, y_train)
+            
             y_test_pred = model.predict(X_test)
 
             test_score = r2_score(y_test_pred, y_test)
 
-            report[list(models.keys())[i]] = test_score
+            report[list(models.keys())[i]] = (test_score, cv.best_params_)
         
         return report
 
